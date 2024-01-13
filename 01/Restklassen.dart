c35f7b0fce721a6369cf7569ed746d2eb56fcc6d
Sebastian Buchner <sebastian.buchner@pm.me>
2024-01-13 14:46:31 +0100

    add reading input for q1

---
+++ 01/Restklassen.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+import 'dart:core';
+
+/**
+ * 
+ */
+
+
+void main() {
+  
+  File file = new File('01/input.txt');
+  List<String> lines = file.readAsLinesSync();
+    
+  // create hills
+  for(String line in lines) {
+    
+  }
+
+}
